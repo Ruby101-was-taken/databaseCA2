3371d136775fddc6055cb50198bffd476c654a9c
Ruby101-was-taken <abusinessemailkinda@gmail.com>
2023-12-04 13:40:24 +0000

    added sample data

--- DATOBASE.sql
+++ DATOBASE.sql
@@ -86,3 +86,37 @@ ALTER TABLE VendorInventory ADD CONSTRAINT VendorInventory_Vendors FOREIGN KEY V
 
 -- End of file.
 
+-- Sample data for Venue table
+INSERT INTO Venue (VenueID, Name, Address) VALUES
+(1, 'Convention Center', '123 Main St'),
+(2, 'Art Expo Hall', '456 Elm St');
+
+-- Sample data for ArtistAlley table
+INSERT INTO ArtistAlley (ArtistAlleyID, VenueID, Name, Capacity) VALUES
+(1, 1, 'Creative Corner', 50),
+(2, 2, 'Artistic Express', 30);
+
+-- Sample data for Vendors table
+INSERT INTO Vendors (VendorID, Name) VALUES
+(1, 'Artistic Wonders'),
+(2, 'Crafty Creations');
+
+-- Sample data for Products table
+INSERT INTO Products (ProductID, Name, Price, Description) VALUES
+(1, 'Painting Kit', 25.99, 'Complete set for artists'),
+(2, 'Sculpture Tools', 12.50, 'High-quality sculpting tools');
+
+-- Sample data for VendorArtistAlleys table
+INSERT INTO VendorArtistAlleys (VendorID, ArtistAlleyID) VALUES
+(1, 1),
+(2, 2);
+
+-- Sample data for VendorInventory table
+INSERT INTO VendorInventory (VendorID, ProductID) VALUES
+(1, 1),
+(2, 2);
+
+-- Sample data for Event table
+INSERT INTO Event (VenueID, VendorID, EventID, Date) VALUES
+(1, 1, 101, '2023-05-15'),
+(2, 2, 102, '2023-06-20');
