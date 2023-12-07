9cc21c862628a9f41cb42dcdc4f33773bd3d9a23
LtConfetti <johncodetalkernally@gmail.com>
2023-12-07 18:20:26 +0000

    added email to vendors, adding sample data

--- DATOBASE.sql
+++ DATOBASE.sql
@@ -44,6 +44,7 @@ CREATE TABLE VendorInventory (
 CREATE TABLE Vendors (
     VendorID int  NOT NULL,
     Name varchar(255)  NOT NULL,
+    Email varchar(255) NOT NULL,
     CONSTRAINT Vendors_pk PRIMARY KEY (VendorID)
 );
 
@@ -93,8 +94,11 @@ INSERT INTO Venue (VenueID, Name, Address) VALUES
 
 -- Sample data for ArtistAlley table
 INSERT INTO ArtistAlley (ArtistAlleyID, VenueID, Name, Capacity) VALUES
-(1, 1, 'Creative Corner', 50),
-(2, 2, 'Artistic Express', 30);
+VALUES (1, 1, 'Adam West Hall', 150),
+VALUES (2, 1, 'Clint East Hall', 320),
+VALUES (3, 1, 'Nolan North Hall', 190),
+VALUES (4, 1, 'James South Hall', 55),
+VALUES (5, 1, 'Centre Hall', 100);
 
 -- Sample data for Vendors table
 INSERT INTO Vendors (VendorID, Name) VALUES
@@ -102,9 +106,32 @@ INSERT INTO Vendors (VendorID, Name) VALUES
 (2, 'Crafty Creations');
 
 -- Sample data for Products table
-INSERT INTO Products (ProductID, Name, Price, Description) VALUES
-(1, 'Painting Kit', 25.99, 'Complete set for artists'),
-(2, 'Sculpture Tools', 12.50, 'High-quality sculpting tools');
+INSERT INTO Products (ProductID, Name, Price, Description)
+  (1, 'Hand-Drawn Art', 10.00, 'Hand drawn pictures by me in an art studio of anime characters'),
+  (2, 'Digital Art Drawings', 25.00, 'Digital designed art of various genres and shows'),
+  (3, 'Wooden Prop Swords', 50.00, 'Handmade and painted wooden swords based on swords from various TV shows'),
+  (4, 'Virtual Reality Experience', 30.00, 'VR simulations for all ages'),
+  (5, 'Comic Book Collectibles', 15.00, 'Rare and vintage comic books'),
+  (6, 'Custom Cosplay Costumes', 80.00, 'Cosplay outfits for various characters characters'),
+  (7, 'Handcrafted Jewelry', 40.00, 'Handmade jewelry pieces'),
+  (8, 'Anime Merchandise', 20.00, 'Official merchandise from anime series'),
+  (9, 'Board Games Galore', 35.00, 'Board games for all ages'),
+  (10, 'Sci-Fi Memorabilia', 45.00, 'Collectibles from science fiction movies'),
+  (11, 'Geeky Gadgets', 60.00, 'Tech gadgets with a geeky and futuristic style'),
+  (12, 'Fantasy Art Prints', 18.00, 'High quality prints of fantasy artwork for your walls'),
+  (13, 'Magic Potions and Elixirs', 12.00, 'Novelty drinks with magical effects'),
+  (14, 'Video Game Merch', 22.00, 'Merchandise from popular video games'),
+  (15, 'DIY Craft Kits', 25.00, 'Craft kits for creating your own decorations'),
+  (16, 'Superhero Capes', 15.00, 'Customizable capes for kids and adults'),
+  (17, 'Handmade Plushies', 30.00, 'Adorable handmade plush toys inspired by various shows'),
+  (18, 'Fandom T-Shirts', 18.00, 'High-quality T-shirts featuring designs from various shows'),
+  (19, 'Collectible Figurines', 40.00, 'Limited edition Funkos of characters from movies and TV shows'),
+  (20, 'Pop Culture Posters', 15.00, 'Posters showcasing your favorite movies'),
+  (21, 'Gaming Accessories', 25.00, 'Accessories for gamers, including mouse pads and headsets'),
+  (22, 'Sci-Fi Books Collection', 12.00, 'A collection of science fiction novels'),
+  (23, 'Fantasy-themed Candles', 20.00, 'Handmade candles with scents inspired by fantasy settings'),
+  (24, 'Manga Artwork Prints', 14.00, 'Art prints featuring illustrations from popular mangas'),
+  (25, 'Convention Exclusive Merch', 30.00, 'Merchandise available exclusively at the convention');
 
 -- Sample data for VendorArtistAlleys table
 INSERT INTO VendorArtistAlleys (VendorID, ArtistAlleyID) VALUES
